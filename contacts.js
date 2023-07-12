551135e700a4c3582fd202a2f57ae532e1dbab17
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-07-12 23:13:17 +0300

    added try...catch wrapper

--- contacts.js
+++ contacts.js
@@ -3,85 +3,80 @@ const path = require('path');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
+async function errorHandling(asyncFunction) {
+  try {
+    return await asyncFunction();
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
+}
+
 //Повертає масив контактів.
 async function listContacts() {
-    try {
-      const data = await fs.readFile(contactsPath);
-      const contacts = JSON.parse(data);
-  
-      console.log('List of contacts:');
-      console.table(contacts);
+  return errorHandling(async () => {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
 
-      return contacts;
-    } catch (error) {
-      console.log('Error: ', error.message);
-    }
-}
-  
-//Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    console.log('List of contacts:');
+    console.table(contacts);
 
-async function getContactById(contactId) {
-  try {
-    const contacts = await listContacts();
-
-    const contact = contacts.find(contact => {
-      if (contact.id === contactId) {
-        console.log(`Contact by id ${contactId}:`);
-        console.table(contact);
-
-        return contact;
-      }
+    return contacts;
   });
-
-  if (contact == null) {
-    console.log(`Contact with id "${contactId}" not found!`);
-  }
-
-  } catch (error) {
-    console.log('Error: ', error.message);
-  }
 }
-  
+
+//Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+async function getContactById(contactId) {
+  return errorHandling(async () => {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (contact) {
+      console.log(`Contact by id ${contactId}:`);
+      console.table(contact);
+
+      return contact;
+    } else {
+      console.log(`Contact with id "${contactId}" not found!`);
+
+      return null;
+    }
+  });
+}
+
 //Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
 async function removeContact(contactId) {
-  try {
+  return errorHandling(async () => {
     const contacts = await listContacts();
-
-    const updatedContacts = contacts.filter(contact => contact.id !== contactId);
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
 
     if (updatedContacts.length === contacts.length) {
       console.log(`Contact with id "${contactId}" not found!`);
+
       return;
     }
 
     console.log(`Contact with id "${contactId}" deleted successfully! New list of contacts:`);
     console.table(updatedContacts);
 
-    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));    
-  } catch (error) {
-    console.log('Error: ', error.message);
-  }
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
+  });
 }
 
-  
 //Повертає об'єкт доданого контакту. 
 async function addContact(name, email, phone) {
-    try {
-      const contacts = await listContacts();
+  return errorHandling(async () => {
+    const contacts = await listContacts();
 
-      const newContact = { id: Date.now().toString(), name, email, phone };
-      contacts.push(newContact);
+    const newContact = { id: Date.now().toString(), name, email, phone };
+    contacts.push(newContact);
 
-      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log('New contact added successfully!');
 
-      console.log('New contact added successfully!');
-      console.table(newContact);
+    console.table(newContact);
+    return newContact;
 
-    } catch (error) {
-      console.log('Error: ', error.message);
-    }
+  });
 }
-
 
 module.exports = {
   listContacts,
